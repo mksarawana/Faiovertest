5a33479193b344c34fbbf0f44edde5bb4086848b
mksarawana <30695056+mksarawana@users.noreply.github.com>
2018-06-21 21:20:10 +0530

    Update inv.py

--- inv.py
+++ inv.py
@@ -20,9 +20,9 @@ import glob
 import re
 
 
-users_list = ['cdknoc', 'adp']
-default_router_password = 'vEtEy01fun!'
-default_enable_password = 'har2$tayN'
+users_list = []
+default_router_password = ''
+default_enable_password = ''
 remarks = []
 output_file_name = ""
 vrf_name = ""
